1de08d3144b8830a3f2655345b5c1a4220e7d9cc
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-05-30 10:54:36 +0200

    Add initial HTML and JavaScript files for user input comparison

---
+++ main.js
@@ -0,0 +1,12 @@
+// l'utente inserisce due numeri in successione, con due prompt
+// il softwe stampa il maggiore 
+
+let numero1 = prompt("primo numero")
+let numero2 = prompt("secondo numero")
+
+if (numero1 > numero2) {
+    console.log("il primo numero è maggiore");
+} else if (numero2 > numero1) {
+    console.log("il secondo numero è maggiore");
+
+}
